9b4e7dfefd6cd09ac62434f3f7a778cf4eff7063
Joseph Greenwell <joeygreenwell@ymail.com>
2021-03-30 18:01:26 -0500

    update

--- devlop/game.js
+++ devlop/game.js
@@ -18,13 +18,43 @@ var questions=[{
     RightA:"10"
 }
 ];
+var question=document.querySelector(".question");
+var answer1=document.querySelector(".answer1");
+var answer2=document.querySelector(".answer2");
+var answer3=document.querySelector(".answer3");
+var answer4=document.querySelector(".answer4");
+var answer=document.querySelector(".answer");
 
 // main quiz function
 function quiz(event){
-    event.preventDefault();
+    // event.preventDefault();
     // removes starting page elements on start
     document.querySelector(".startingPage").style.display='none';
+    document.querySelector(".quizPage").style.display='contents';
     timerStart();
+
+    if(i=0,i<4){
+    question.textContent=questions[i]["Q"];
+    answer1.textContent=questions[i]["ans"][0];
+    answer2.textContent=questions[i]["ans"][1];
+    answer3.textContent=questions[i]["ans"][2];
+    answer4.textContent=questions[i]["ans"][3];
+    answer1.setAttribute('data-value', questions[i]['ans'][0]);
+    answer2.setAttribute('data-value', questions[i]['ans'][1]);
+    answer3.setAttribute('data-value', questions[i]['ans'][2]);
+    answer4.setAttribute('data-value', questions[i]['ans'][3]);
+    answer.addEventListener("click", function(event){
+        event.preventDefault();
+        console.log(event.currentTarget.textContent);
+        
+
+    });
+    // if item click ==questions[i][RightA]then say right and i++
+    // else if item click !==questions[i][RightA]then say wrong, sec-3, i++
+    }else{
+        gameOver();
+    }
+
 
 }
 
@@ -34,12 +64,18 @@ function timerStart(){
     var sec = 20;
     var timer = setInterval(function(){
         timer.textContent='00:'+sec;
-        console.log(sec);
+        // console.log(sec);
         sec--;
         if (sec < 0) {
             clearInterval(timer);
+            gameOver();
         }
     }, 1000);
+};
+
+// performs the end of quiz events
+function gameOver(){
+    document.querySelector(".finished").style.display='contents';
 };
 
 
