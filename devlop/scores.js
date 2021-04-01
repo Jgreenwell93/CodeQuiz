c16eb9580067e29a1bc6d370db919ae1a4bb7a99
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-01 01:21:22 -0500

    repairing highscore display function

--- devlop/scores.js
+++ devlop/scores.js
@@ -1,24 +1,23 @@
 var scorelist=document.querySelector(".scoreslist");
 var s=0;
-var scores=localStorage.getItem(localStorage.value);
-console.log(scores);
+
 
 // creates a loop to grab items from local storage and display scores
-function scoreLog(s){
-if (s<=localStorage.length) {
-var score=scorelist.createElement("li");
-score.textContent=localStorage.getItem(s);
-scorelist.appendChild(score);
-s++;
-scoreLog();
-}else{
-    return;
-};
-};
+for (i = 0; i < localStorage.length; i++) {
+   var x = localStorage.key(i);
+   var z=localStorage.getItem(x);
+   var y=JSON.parse(x);
+    var score=document.createElement("li");
+    score.textContent=y + " - " + z;
+    scorelist.appendChild(score);
+  };
 
 
 function scoresWipe(){
 localStorage.clear();
+while( scorelist.firstChild ){
+    scorelist.removeChild( scorelist.firstChild );
+  }
 };
 
 document.querySelector(".clearScores").addEventListener("click",scoresWipe);
